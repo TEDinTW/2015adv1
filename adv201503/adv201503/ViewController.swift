569920b2a8014b3b3c4e798e0b2e31f854459ee3
Connor <tommy@Connor.local>
2015-03-02 19:55:25 +0800

    ConrTG

--- adv201503/adv201503/ViewController.swift
+++ adv201503/adv201503/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         println("aaaa")
         // test
+        
+        println("BY ConrTG")
     }
 
     override func didReceiveMemoryWarning() {
