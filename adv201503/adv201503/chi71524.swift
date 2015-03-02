ca35bdc2a64c7603d9445618c451e2bf37a21f26
ios <ios@Mac-10.local>
2015-03-02 20:17:35 +0800

    added by chi71524

---
+++ adv201503/adv201503/chi71524.swift
@@ -0,0 +1,33 @@
+//
+//  chi71524.swift
+//  adv201503
+//
+//  Created by ios on 2015/3/2.
+//  Copyright (c) 2015年 liangjason. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        println("aaaa")
+        // test
+        // Hello by Kamewa
+        
+        println("BY ConrTG")
+        
+        println("lan8177")
+        println("chi71524")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
