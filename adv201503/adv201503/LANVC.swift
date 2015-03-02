f2e2146c1da7314d69d31006de4d69d49345ca41
ios <gh@Mac-15.local>
2015-03-02 20:17:03 +0800

    added LANVC

--- adv201503/adv201503/LANVC.swift
+++ adv201503/adv201503/LANVC.swift
@@ -1,32 +1,29 @@
 //
-//  ViewController.swift
+//  LANVC.swift
 //  adv201503
 //
 //  Created by ios on 2015/3/2.
 //  Copyright (c) 2015年 liangjason. All rights reserved.
 //
 
+
 import UIKit
 
-class ViewController: UIViewController {
-
+class LANVC: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        println("aaaa")
-        // test
-        
-        println("BY ConrTG")
-        
-        println("lan8177")
-        println("hehehe")
-    }
 
+        // test
+        println("lan8177")
+
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
