fd55eb5d7941a0e21e88149607728948801ec9e7
ios <ios@Mac-02.local>
2015-03-02 20:17:32 +0800

    加入 ＴＯＮＹＶＣ

---
+++ adv201503/adv201503/TONYVC.swift
@@ -0,0 +1,35 @@
+//
+//  TONYVC.swift
+//  adv201503
+//
+//  Created by ios on 2015/3/2.
+//  Copyright (c) 2015年 liangjason. All rights reserved.
+//
+
+import UIKit
+
+class TONYVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        println("aaaa")
+        // test
+        // Hello by Kamewa
+        
+        println("BY ConrTG")
+        
+        println("lan8177")
+        
+        println("BY TED")
+        println("tony")
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
