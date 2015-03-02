7dedcd8de3e8095ae1702bdde31b859b893b84e3
ios <ios@Mac-14.local>
2015-03-02 20:57:45 +0800

    add new file

---
+++ adv201503/adv201503/Attente8098.swift
@@ -0,0 +1,38 @@
+//
+//  Attente8098.swift
+//  adv201503
+//
+//  Created by ios on 2015/3/2.
+//  Copyright (c) 2015年 liangjason. All rights reserved.
+//
+
+import UIKit
+
+class Attente8098: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        println("Test")
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
