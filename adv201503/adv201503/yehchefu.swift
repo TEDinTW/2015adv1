890241bc775d99054cbc2c9656d13a9a614463f7
ios <ios@Mac-07.local>
2015-03-02 20:17:11 +0800

    1123

---
+++ adv201503/adv201503/yehchefu.swift
@@ -0,0 +1,26 @@
+//
+//  yehchefu.swift
+//  adv201503
+//
+//  Created by ios on 2015/3/2.
+//  Copyright (c) 2015年 liangjason. All rights reserved.
+//
+
+import UIKit
+
+class yehchefu: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let a = "BY ConrTG"
+        let b = "lan8177"
+        var c = "a+b"
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
